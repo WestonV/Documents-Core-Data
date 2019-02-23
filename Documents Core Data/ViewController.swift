7a823c4c777c14978d0eb008f29411e0bc268e06
Weston Verhulst <w2visme@gmail.com>
2019-02-22 19:49:43 -0600

    created project

---
+++ Documents Core Data/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Documents Core Data
+//
+//  Created by Weston Verhulst on 2/22/19.
+//  Copyright © 2019 Weston Verhulst. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
